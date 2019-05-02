a92f22c316030abd9b6f9a217178e46ee2d233f2
Péter Kalicz <kaliczp@gmail.com>
2019-05-02 13:47:52 +0200

    Correction of current year

--- idosor.R
+++ idosor.R
@@ -3,7 +3,7 @@ teljes <- read.csv("NH.Ts+dSST.csv", skip=1)
 teljes[1,14]
 éves <- as.numeric(as.character(teljes[, "J.D"]))
 plot(éves)
-temp.df <- data.frame(ido = 1880:2018,t = éves)
+temp.df <- data.frame(ido = 1880:2019,t = éves)
 temp.df <- temp.df[-nrow(temp.df),]
 # Nem-paraméteres módszer lokális regresszió
 temp.lowess <- lowess(temp.df)
