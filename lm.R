29c0f4f1a654ee0e121311173a8638e6ce4f0deb
Péter Kalicz <kaliczp@gmail.com>
2019-05-02 12:37:36 +0200

    Some typo

--- lm.R
+++ lm.R
@@ -7,10 +7,11 @@ abline(a=0,b=2) # valós modell
 
 
 elso.lm = lm(y ~ x) # tengelymetszettel
-lm(y ~ x -1) # tengelymetszettel
+lm(y ~ x - 1) # tengelymetszet nélkül
 
 elso.lm
+elso.lm$coefficients # Paraméterek
 abline(elso.lm, col=2)
-elso.lm$coefficients+1
+elso.lm$coefficients[1]
 coef(elso.lm)
 summary(elso.lm)
