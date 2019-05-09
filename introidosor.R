5d30b3100c0471eb9a55da153180c034d5f1c568
Péter Kalicz <kaliczp@gmail.com>
2019-05-09 13:00:55 +0200

    Haladó mozgó átlag

--- introidosor.R
+++ introidosor.R
@@ -16,3 +16,13 @@ lines(co2,lwd=2)
 
 ##Trend idősor nemparamétere szűrővel
 co2.ma3=filter(co2,c(1/3,1/3,1/3)) #Mozgó átlag 3 e. ablak
+
+co2.ma3=filter(co2,c(1/3,1/3,1/3)) #Mozgó átlag 3 e. ablak
+co2.ma4=filter(co2,c(rep(1/4,4))) #Mozgó átlag 4 e. ablak
+co2.ma4jav=filter(co2,c(1/8,rep(1/4,3),1/8)) #Mozgó átlag javított 4 e. ablak
+co2.ma12=filter(co2,c(1/24,rep(1/12,11),1/24)) #Mozgó átlag 12 e. ablak
+lines(co2.ma12,col=3)
+
+##Dekompozíció mozgóátlaggal
+co2.dec <- decompose(co2)
+plot(co2.dec)
