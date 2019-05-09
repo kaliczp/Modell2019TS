1f385e4ce4a56d3c021b1b8229af758a9ec29d3e
Péter Kalicz <kaliczp@gmail.com>
2019-05-09 13:08:27 +0200

    SZENNYVÍZ

--- introidosor.R
+++ introidosor.R
@@ -16,8 +16,7 @@ lines(co2,lwd=2)
 
 ##Trend idősor nemparamétere szűrővel
 co2.ma3=filter(co2,c(1/3,1/3,1/3)) #Mozgó átlag 3 e. ablak
-<<<<<<< HEAD
-
+lines(co2.ma3, col=2)
 co2.ma3=filter(co2,c(1/3,1/3,1/3)) #Mozgó átlag 3 e. ablak
 co2.ma4=filter(co2,c(rep(1/4,4))) #Mozgó átlag 4 e. ablak
 co2.ma4jav=filter(co2,c(1/8,rep(1/4,3),1/8)) #Mozgó átlag javított 4 e. ablak
@@ -27,6 +26,15 @@ lines(co2.ma12,col=3)
 ##Dekompozíció mozgóátlaggal
 co2.dec <- decompose(co2)
 plot(co2.dec)
-=======
-lines(co2.ma3, col=2)
->>>>>>> eb90415c00412d7243529362f95534a57cf7da15
+
+### Nem biztos, hogy ez a hozam!!!!
+szv <- scan("szennyv.txt")
+tt2time <- seq(ISOdatetime(2007,4,10,13,46,00),ISOdatetime(2007,4,20,00,24,00),by="2 mins")
+plot(data.frame(tt2time,szv))
+
+szv.ts <- ts(szv, freq = 24*30)
+plot(szv.ts) # 2 perces mintavételezés
+
+szv.dec <- decompose(szv.ts)
+plot(szv.dec)
+
